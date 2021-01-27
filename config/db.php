8bc8a0c9abf8ce2650a3310c7b487e2f71787f1e
WeresD <weresd48@yandex.ru>
2021-01-27 13:56:00 +0300

    Init

---
+++ config/db.php
@@ -0,0 +1,21 @@
+<?php
+
+use yii\helpers\ArrayHelper;
+
+$db = [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
+
+return ArrayHelper::merge(
+    $db,
+    require __DIR__ . '/db-local.php'
+);
